de04edc3e0c2e054fbaf45d8fdca468793d902bc
Marius Butuc <marius.butuc@gmail.com>
2013-03-24 15:22:14 -0400

    Some Basic Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 RealtimeFaye::Application.routes.draw do
+  # get "sessions/new"
+  # get "sessions/create"
+  get  '/login' => 'sessions#new',    as: :login
+  post '/login' => 'sessions#create', as: :login
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
