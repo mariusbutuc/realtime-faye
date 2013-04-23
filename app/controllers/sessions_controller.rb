3c594ea46c351fded08f57b89d4169ff9820f0d9
Marius Butuc <marius.butuc@gmail.com>
2013-04-23 01:04:41 -0400

    As an inviting user, I want to join the scene as soon as my invitee joins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,18 +9,21 @@ class SessionsController < ApplicationController
     scene_id = params[:scene_id]
 
     unless scene_id
-      # create new character & scene
+      # Create new character & scene
       random_scene_starter = Starter.find Starter.pluck(:id).sample
       scene = Scene.new
       scene.starter = random_scene_starter
       scene.characters.build nickname: params[:username]
     else
-      # join existing scene
+      # Join existing scene
       scene = Scene.find scene_id
       scene.characters.build nickname: params[:username]
     end
 
     if scene.save!
+      # Let the first character know we joined, by refreshing his waiting page.
+      # This will in turn send him directly to start Writing the Scene.
+      PrivatePub.publish_to "/scenes/#{scene_id}/waiting", { marius_says: 'refresh' } if scene_id
       redirect_to scene_path scene
     else
       # TODO: add error message
