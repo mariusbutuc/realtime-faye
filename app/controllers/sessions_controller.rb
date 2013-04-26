871bf81a0e59021fc143fa388d54c8fe7438c2b8
Marius Butuc <marius.butuc@gmail.com>
2013-04-25 22:13:50 -0400

    Character names should be all uppercase

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:username] = params[:username]
+    nickname = params[:username].upcase
+    session[:username] = nickname
     scene_id = params[:scene_id]
 
     unless scene_id
@@ -13,11 +14,11 @@ class SessionsController < ApplicationController
       random_scene_starter = Starter.find Starter.pluck(:id).sample
       scene = Scene.new
       scene.starter = random_scene_starter
-      scene.characters.build nickname: params[:username]
+      scene.characters.build nickname: nickname
     else
       # Join existing scene
       scene = Scene.find scene_id
-      scene.characters.build nickname: params[:username]
+      scene.characters.build nickname: nickname
     end
 
     if scene.save!
